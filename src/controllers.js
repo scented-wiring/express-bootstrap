e0177285f55cb74ce624ccb4953a71efdbba7201
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-08-02 19:28:05 +0100

    refactored code

--- src/controllers.js
+++ src/controllers.js
@@ -1,17 +1,11 @@
 // eslint-disable-next-line
-const request = require('request');
 const axios = require('axios');
 
 const jokeController = (req, res) =>
-  request('https://api.icndb.com/jokes', (error, jokesAPIresponse) => {
-    if (error) {
-      return res.status(error.statusCode).send({ error: error.message });
-    }
-
-    const parsedResponse = JSON.parse(jokesAPIresponse.body);
-
-    res.send({ jokes: parsedResponse.value });
-  });
+  axios
+    .get('https://api.icndb.com/jokes')
+    .then(response => res.send({ jokes: response.data.value }))
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 
 const randomController = (req, res) =>
   axios
@@ -24,15 +18,13 @@ const randomController = (req, res) =>
 const personalController = async (req, res) => {
   const { first, last } = req.params;
 
-  try {
-    const response = await axios.get(
+  return axios
+    .get(
       `https://api.icndb.com/jokes/random?firstName=${first}&lastName=${last}&exclude=[explicit]`,
-    );
+    )
 
-    return res.send({ personalJoke: response.data.value });
-  } catch (error) {
-    return res.status(error.statusCode).send({ error: error.message });
-  }
+    .then(response => res.send({ personalJoke: response.data.value }))
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 };
 
 module.exports = {
